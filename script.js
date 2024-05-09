b6807b0cfe067fab92ecabd2199a25d4ae574595
rahulranjann <rahul1913111054@gmail.com>
2024-05-09 15:14:56 +0530

    google analytics. added

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function clickFun(element) {
-  const pages = document.querySelectorAll(".pages");
-  pages.forEach(function (page) {
-    page.classList.remove("active");
+    const pages = document.querySelectorAll(".pages");
+    pages.forEach(function (page) {
+      page.classList.remove("active");
+    });
+    console.log(element.dataset.active);
+    const sect = document
+      .getElementById(element.dataset.activePage)
+      .classList.add("active");
+  }
+  
+  var typed = new Typed(".typing", {
+    strings: [
+      "Rahul",
+      " a Friend.",
+      "a Programmer.",
+      "a Gamer.",
+      "a Student.",
+      "Developer.",
+    ],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 50,
   });
-  console.log(element.dataset.active);
-  const sect = document
-    .getElementById(element.dataset.activePage)
-    .classList.add("active");
-}
-
-var typed = new Typed(".typing", {
-  strings: [
-    "Rahul",
-    " a Friend.",
-    "a Programmer.",
-    "a Gamer.",
-    "a Student.",
-    "Developer.",
-  ],
-  loop: true,
-  typeSpeed: 50,
-  backSpeed: 50,
-});
+  
