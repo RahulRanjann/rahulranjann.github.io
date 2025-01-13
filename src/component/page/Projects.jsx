c528d284d153cdf98e9d0f5d16126ddd65a9f528
rahul ranjann <rahul1913111054@gmail.com>
2025-01-13 17:07:02 +0530

    project link and desc. added

--- src/component/page/Projects.jsx
+++ src/component/page/Projects.jsx
@@ -8,31 +8,26 @@ function Projects() {
         {
             // img: githubImg,
             img: require('../../assets/githubApi.png'),
-            title: "Github Api",
-            desc: "This is a brief description of the project. lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, voluptas. Nemo, ipsam.",
-            techStack: "HTML, CSS, JavaScript, Tailwind CSS, Node.js",
-            websiteLink: "https://www.example.com",
-            githubLink: "https://github.com/example",
+            title: "GitHub Profile Finder",
+            desc: "A web app that uses the GitHub API to fetch profile details by entering a username. It displays user info like name, bio, profile picture, repositories, followers, and more. Built with React.js, it features dynamic data fetching, error handling, and a clean UI for an enhanced user experience.",
+            techStack: "HTML, CSS, JavaScript, Tailwind, React, GitHub API",
+            websiteLink: "https://rahulranjann.github.io/ReactJs-main/",
+            githubLink: "https://github.com/RahulRanjann/ReactJs-main/blob/main/about-react-hooks/src/Component/GitHubApi.jsx",
             colabaterName: [
                 {
                     name: "Rahul Ranjan",
                     img: Rahul, 
                     colabaterLink: "https://github.com/rahulranjann"
                 },
-                {
-                    name: "Om Prakash Bharti",
-                    img: Om,
-                    colabaterLink: "https://github.com/samnickgammer"
-                }
             ]
         },
         {
             img: require('../../assets/goMilestone.png'),
-            title: "GoMilestone",
-            desc: "This is another project description.",
-            techStack: "React, Express.js, MongoDB, Tailwind CSS",
-            websiteLink: "https://www.example2.com",
-            githubLink: "https://github.com/example2",
+            title: "Go Milestone",
+            desc: "Developed the official GoMilestone website using HTML, CSS, JavaScript, and MDB5. Integrated a Google Map to display the company’s location and added a country code feature in the contact us form to enhance phone number input. The website was successfully hosted on GitHub Pages for easy access and deployment.",
+            techStack: "HTML, CSS, JavaScript, MDB5, Google Map, GitHub",
+            websiteLink: "https://rahulranjann.github.io/JavaScriptProject/GoMile%20Stone/index.html",
+            githubLink: "https://github.com/RahulRanjann/JavaScriptProject/tree/master/GoMile%20Stone",
             colabaterName: [
                 {
                     name: "Rahul Ranjan",
@@ -44,15 +39,20 @@ function Projects() {
         {
             img: require('../../assets/tour&traver.png'),
             title: "TourTravel",
-            desc: "This is another project description. lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, voluptas. Nemo, ipsam.", 
-            techStack: "React, Express.js, MongoDB, Tailwind CSS, Node.js, html, css",
-            websiteLink: "https://www.example2.com",
-            githubLink: "https://github.com/example2",
+            desc: "A website where users can sign up, log in, and book tickets via a form. Firebase handles authentication and database management. The site integrates Tawk.to for real-time chat support. Built with HTML, CSS (Bootstrap), and JavaScript for a responsive and interactive user experience.", 
+            techStack: "Firebase, JavaScript, gitHub, HTML, CSS",
+            websiteLink: "https://rahulranjann.github.io/JavaScriptProject/tour%20and%20travel%20website/web.html#book",
+            githubLink: "https://github.com/RahulRanjann/JavaScriptProject/tree/master/tour%20and%20travel%20website",
             colabaterName: [
                 {
                     name: "Rahul Ranjan",
                     img: Rahul,
                     colabaterLink: "https://github.com/rahulranjann"
+                },
+                {
+                    name: "Om Prakash",
+                    img: Om,
+                    colabaterLink: "https://github.com/samnickgammer"
                 }
             ]
         }
